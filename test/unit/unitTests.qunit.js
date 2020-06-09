c3642e49c8a75ce21ab100895a57d723ea22d46e
S0021493556 <vijay.patne@wipro.com>
2020-06-09 09:21:23 +0000

    Import content for version 1.0.10

---
+++ test/unit/unitTests.qunit.js
@@ -0,0 +1,1 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["CreditDebit/UploadCreditDebitMemo/test/unit/AllTests"],function(){QUnit.start()})});
